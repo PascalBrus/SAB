01f0d6382ebf94c69e3ac294fd2ac1d12735b7be
Pascal_Brus <email@example.com>
2025-02-14 13:13:04 +0100

    finished meta data table, might change the number array situation in the future

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from classes.misc import colors
 
 ## Literal option muste be File name of sorting Function origin
 ## Sorting function must have 1 and only 1 Entry point named sort()
-optionArray: Literal["bubble","merge","insert", "quick", "selection", "quickIter", "quickIter2"] = ["bubble", "merge"]
+optionArray: Literal["bubble","merge","insert", "quick", "selection", "quickIter", "quickIter2"] = ["bubble"]
 metricsArray: Literal["minimal", "default", "extended", "all"] = "default"
 
 ranges = {
-  "count": 10,
+  "count": 50,
   "lowRange": -50,
   "highRange": 50
 }
 #benchmark = Benchmark(optionArray, ranges, "minimal")
 #benchmark = Benchmark(optionArray, ranges, "default")
-#benchmark = Benchmark(optionArray, ranges, "extended")
-benchmark = Benchmark(optionArray, ranges, "all")
+benchmark = Benchmark(optionArray, ranges, "extended")
+#benchmark = Benchmark(optionArray, ranges, "all")
 
 
 print(f"{colors.WARNING}Printed all avialable Steps{colors.ENDC}")
