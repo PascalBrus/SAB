f99c74de8b2998b57d683639655d6946ca95a95c
Pascal_Brus <email@example.com>
2025-02-28 07:48:17 +0100

    argparse works but not done yet by a long shot

--- classes/misc.py
+++ classes/misc.py
@@ -1,13 +1,10 @@
 import time
-
-
 class colors:
   OKGREEN = '\033[92m'
   WARNING = '\033[93m'
   FAIL = '\033[91m'
   ENDC = '\033[0m'
   
-
 class Timestamp:
   def __init__(self):
     self.start()
@@ -17,3 +14,17 @@ class Timestamp:
   def end(self):
     self.endTime = time.time()
     return (self.endTime - self.startTime)*1000*1000
+
+
+class validateArgs:
+  def __init__(self, ogArgs, args):
+    self.ogArgs = set(ogArgs)
+    self.args = args
+  
+  def validate(self) -> bool:
+    for arg in self.args:
+      if arg not in self.ogArgs:
+        return False
+    return True
+
+
---
+++ classes/validateArgs.py
@@ -0,0 +1,1 @@
+#in misc for now
